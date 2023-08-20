907c57c9d866dbdcc385f747cde8e74cff80b5d9
kapillohanii <kapillohani023@gmail.com>
2023-08-20 12:35:51 +0530

    initial commit

---
+++ sql/queries/DimDate.sql
@@ -0,0 +1,25 @@
+-- cleansed DimDate
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek] As Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear] AS WeekNo, 
+  [EnglishMonthName] AS Month, 
+  LEFT([EnglishMonthName], 3) AS MonthShort, 
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2022].[dbo].[DimDate] 
+WHERE 
+  CalendarYear >= 2021
